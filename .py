fdbba654c4ac1c856ad0d7bd66f1e64012ad5eb7
riserishitha <rishitha.n@kalvium.community>
2024-08-16 23:12:29 +0530

    journal-16/8

--- .py
+++ .py
@@ -113,3 +113,49 @@ class Solution:
             current = current.next        
         return self.reverseList(reversed_head)
 
+# Anagram (16/8):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(s) == sorted(t):
+            return True
+        else :
+            return False        
+
+# hunger strike : (16/8) :
+
+num_children = int(input())
+greedy_factors = list(map(int, input().split()))
+num_cookies = int(input())
+cookie_sizes = list(map(int, input().split()))
+greedy_factors.sort()
+cookie_sizes.sort()
+content_children = 0
+cookie_index = 0
+for i in range(num_children):
+    if cookie_index < num_cookies and cookie_sizes[cookie_index] >= greedy_factors[i]:
+        content_children += 1
+        cookie_index += 1
+print(content_children)
+
+# oddeven linked list :
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def oddEvenList(self, head):
+        if not head or not head.next:
+            return head
+        odd = head
+        even = head.next
+        even_head = even  
+        
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next        
+            even.next = odd.next 
+            even = even.next      
+        odd.next = even_head 
+        return head
